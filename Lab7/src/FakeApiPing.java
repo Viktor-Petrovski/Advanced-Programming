479b23e758d5c005de44e625b49fbd9a7e5b376c
petrovski <viktor.petrovski04@gmail.com>
2026-01-28 21:02:36 +0100

    Lab 7 Additional Requirement 2

--- Lab7/src/FakeApiPing.java
+++ Lab7/src/FakeApiPing.java
@@ -71,9 +71,39 @@ public class FakeApiPing {
                 f.cancel(true); // !important
                 results.add(new ApiResult(requestId, false, "TIMEOUT"));
             } catch (ExecutionException | InterruptedException e) {
-//                results.add(new ApiResult(requestId, false, "FAILED"));
+                System.out.println(e.getMessage());
             }
         }
+
+//         Потребно е да се имплементира механизам кој ќе го ограничи бројот на API повици што се извршуваат паралелно.
+//         По детекција на првиот API повик кој не завршил во дозволеното време,
+//         сите останати активни и неисполнети повици ќе бидат прекинати
+//        API повиците кои се прекинати мора да бидат означени како неуспешни (FAILED)
+//
+//        boolean errorFlag = false;
+//        for (int i = 0; i < futures.size(); i++) {
+//            Future<ApiResult> f = futures.get(i);
+//            int requestId = i + 1;
+//
+//            if (errorFlag) {
+//                results.add(new ApiResult(requestId, false, "FAILED"));
+//                continue;
+//            }
+//
+//            try {
+//                results.add(f.get(timeoutMillis, TimeUnit.MILLISECONDS));
+//            } catch (ExecutionException | InterruptedException e) {
+//                throw new RuntimeException(e);
+//            } catch (TimeoutException e) {
+//                f.cancel(true);
+//                results.add(new ApiResult(requestId, false, "TIMEOUT"));
+//                errorFlag = true;
+//            }
+//
+//        }
+
+        // for this additional requirement I implemented a flag that is set to true once one future times out
+        // then the rest are marked as failed
 
         executor.shutdown();
 
